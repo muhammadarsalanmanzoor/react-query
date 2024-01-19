59b7d8b0778767687bd883e2837dffe3f7430f51
muhammadarsalanmanzoor <arsalan@get-licensed.co.uk>
2024-01-19 15:15:11 +0500

    learned how to transform response data

--- src/pages/RQSuperHeroesPage.jsx
+++ src/pages/RQSuperHeroesPage.jsx
@@ -3,52 +3,21 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 /**
- *  callBacks with useQuery:
- *  - callBacks with useQuery when we are dealing with data fetching 
- *    sometime we might want to perform a side effect when the query 
- *    completes an example could be opening a modal or navigating to
- *    a different route or even displaying toast notifications to cater
- *    to these scenarios react query let's us specify success and error
- *    callbacks as configurations or options to the useQuery hook,
- *    let's see how to add them in our component;
- *  
- *  - First we need to define two functions which will be called when
- *    query succeeds or when it fails.
- * 
- *  - The first one let's call it onSuccess, this function will be called
- *    when the query successfully fetches data, similarly let's define 
- *    onError function which gets called when the query encounters an error
- *    while trying to fetch the data, to attach these functions to our 
- *    super-heroes query all we have to do is to specify onsuccess and onerror
- *    configurations, so the third argument in useQuery which is an object 
- *    we specify onsuccess and onerror callback, now lets' save the file
- *    and let's see if this works. Navigate to RQ Super heroes page and we
- *    see the success callback log message and go back to vs code and change 
- *    the url to superheroes11111 head back to the browser navigate to RQ
- *    Super heroes the query retries three times before calling the onError
- *    callback. What is also worth noting is react query automatically 
- *    injects the data that has been fetched or the error that was encountered
- *    into these callback params so we can specify data as params into onsuccess
- *    and similarly error param to onerror callback 
- *
- 
- *
+ *  Data Transformation:
+ *  - We have scenario of needing to transform data into a
+ *    format that the front end component can consume, to
+ *    help with such scenarios react query provides a select
+ *    configuration option which we can specify on the useQuery
+ *    hook. Let's see how it works.
  *
  */
 const RQSuperHeroesPage = () => {
-  const [isRefetchInterval, setIsRefetchInterval] = useState(3000);
   const onSuccess = (data) => {
     console.log('Perform side effect after data fetching...', data);
-
-    if (data?.data.length >= 4) {
-      setIsRefetchInterval(false);
-    }
   };
 
   const onError = (error) => {
     console.log('Perform side effect after data encountering error...', error);
-
-    setIsRefetchInterval(false);
   };
 
   const { isLoading, data, isError, error, isFetching } = useQuery(
@@ -57,10 +26,19 @@ const RQSuperHeroesPage = () => {
       return axios.get('http://localhost:4000/superheroes');
     },
     {
-      refetchInterval: isRefetchInterval,
-
       onSuccess: onSuccess,
       onError: onError,
+      // for data transformation we specify an option or a configuration
+      // called select, a select is a function which automatically receives
+      // the api data as an argument, data in our case is basically the response
+      // let's map over data array extracting just the hero name
+      select: (data) => {
+        const superHeroNames = data.data.map((hero) => hero.name);
+
+        // what this does is change the destructured data to an array of super
+        // hero names which means we need to update our JSX.
+        return superHeroNames;
+      },
     }
   );
 
@@ -77,8 +55,12 @@ const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQSuperHeroesPage</h2>
-      {data?.data.map((hero) => {
+      {/* {data?.data.map((hero) => {
         return <div key={hero.id}>{hero.name}</div>;
+      })} */}
+
+      {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
       })}
     </>
   );
