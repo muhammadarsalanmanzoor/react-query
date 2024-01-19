5a728358dff72667f6f7a8416d2ea7f41e205ab4
muhammadarsalanmanzoor <arsalan@get-licensed.co.uk>
2024-01-19 15:27:17 +0500

    created a custom hook for useQuery hook

---
+++ src/hooks/useSuperHeroes.js
@@ -0,0 +1,23 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const useSuperHeroes = (onSuccess, onError) => {
+  return useQuery(
+    'super-heroes',
+    () => {
+      return axios.get('http://localhost:4000/superheroes');
+    },
+    {
+      onSuccess: onSuccess,
+      onError: onError,
+
+      select: (data) => {
+        const superHeroNames = data.data.map((hero) => hero.name);
+
+        return superHeroNames;
+      },
+    }
+  );
+};
+
+export default useSuperHeroes;
--- src/pages/RQSuperHeroesPage.jsx
+++ src/pages/RQSuperHeroesPage.jsx
@@ -1,16 +1,5 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useQuery } from 'react-query';
+import useSuperHeroes from '../hooks/useSuperHeroes';
 
-/**
- *  Data Transformation:
- *  - We have scenario of needing to transform data into a
- *    format that the front end component can consume, to
- *    help with such scenarios react query provides a select
- *    configuration option which we can specify on the useQuery
- *    hook. Let's see how it works.
- *
- */
 const RQSuperHeroesPage = () => {
   const onSuccess = (data) => {
     console.log('Perform side effect after data fetching...', data);
@@ -20,26 +9,9 @@ const RQSuperHeroesPage = () => {
     console.log('Perform side effect after data encountering error...', error);
   };
 
-  const { isLoading, data, isError, error, isFetching } = useQuery(
-    'super-heroes',
-    () => {
-      return axios.get('http://localhost:4000/superheroes');
-    },
-    {
-      onSuccess: onSuccess,
-      onError: onError,
-      // for data transformation we specify an option or a configuration
-      // called select, a select is a function which automatically receives
-      // the api data as an argument, data in our case is basically the response
-      // let's map over data array extracting just the hero name
-      select: (data) => {
-        const superHeroNames = data.data.map((hero) => hero.name);
-
-        // what this does is change the destructured data to an array of super
-        // hero names which means we need to update our JSX.
-        return superHeroNames;
-      },
-    }
+  const { isLoading, data, isError, error, isFetching } = useSuperHeroes(
+    onSuccess,
+    onError
   );
 
   console.log({ isLoading, isFetching });
@@ -55,9 +27,6 @@ const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQSuperHeroesPage</h2>
-      {/* {data?.data.map((hero) => {
-        return <div key={hero.id}>{hero.name}</div>;
-      })} */}
 
       {data.map((heroName) => {
         return <div key={heroName}>{heroName}</div>;
